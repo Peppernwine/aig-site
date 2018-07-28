98dc6ec44432ac7f2f64deb891a19720ee51e58c
peppernwine <corporate@peppernwine.com>
2018-07-27 21:39:46 -0400

    Added ubuntu install script

---
+++ install-apps.sh
@@ -0,0 +1,42 @@
+sudo apt update && sudo apt upgrade
+sudo apt install tasksel
+sudo tasksel install lamp-server
+
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get update
+
+sudo apt-get install php5.6
+sudo apt-get install php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml
+
+sudo a2enmod rewrite
+sudo systemctl restart apache2
+
+sudo a2dismod php5.6
+sudo a2enmod php7.0
+sudo service apache2 restart
+
+sudo apt-get install php5.6-curl
+sudo php composer.phar update
+
+
+#enable mode rewrite..
+sudo a2enmod rewrite
+sudo systemctl restart apache2
+
+
+# update /etc/apache2/sites-available/000-default.conf
+#<VirtualHost *:80>
+#    <Directory /var/www/html>
+#        Options Indexes FollowSymLinks MultiViews
+#        AllowOverride All
+#        Require all granted
+#    </Directory>
+
+# update /etc/apache2/apache2.conf
+#</VirtualHost>
+
+#<Directory /var/www/>
+#    Options Indexes FollowSymLinks
+#    AllowOverride All
+#    Require all granted
+#</Directory>
